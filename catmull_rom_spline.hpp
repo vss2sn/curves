3b8cfd9abba5fc3c8657e05b76ddb3e890b56e11
vss <28676655+vss2sn@users.noreply.github.com>
2022-03-27 19:02:02 -0700

    Add Catmull-Rom splines

---
+++ catmull_rom_spline.hpp
@@ -0,0 +1,62 @@
+#ifndef CATMULL_ROM_SPLINE_HPP
+#define CATMULL_ROM_SPLINE_HPP
+
+#include <array>
+
+// Note: a catmull-rum spline iss basically
+// a set of hermite splines connecting sequental points,
+// using tao to calculate the slopes at each ot the intermediate points
+
+// n_points here is n_points per segment not total number of points
+template<size_t N, size_t n_points, size_t dimensions>
+class CatmullRomSpline {
+public:
+  explicit CatmullRomSpline(const std::array<Point<dimensions>, N>& p, const float tao) : p(p), tao(tao) {
+    const float interval = (1.)/n_points;
+    std::array<std::array<float, 4>,  n_points+1> coefficients;
+    const std::array<std::array<float,4>, 4> tao_matrix {
+      std::array<float,4>{0., 1., 0., 0.},
+      std::array<float,4>{-tao, 0., tao, 0.},
+      std::array<float,4>{2.*tao, tao-3., 3.-2.*tao, -tao},
+      std::array<float,4>{-tao, 2.-tao, tao-2., tao}
+    };
+    float u = 0;
+    for (int i = 0; i < n_points + 1; i++) {
+      const std::array<float, 4> u_matrix {1, u, u*u, u*u*u};
+      for (int j = 0; j < 4; j++) {
+        coefficients[i][j] = 0;
+        for (int k = 0; k < 4;  k++) {
+          coefficients[i][j] += u_matrix[k] * tao_matrix[k][j];
+        }
+      }
+      u += interval;
+    }
+
+    for (int i = 0; i <= N-4; i++) {
+      for (int j = 0; j < n_points + 1; j++) {
+        for (int m = 0; m < dimensions; m++) {
+          points[(n_points + 1) * i+j][m] = 0;
+          for (int k = 0; k < 4; k++) {
+            points[(n_points + 1) * i+j][m] += coefficients[j][k] * p[i+k][m];
+          }
+        }
+      }
+    }
+  }
+
+  void print() {
+    for (const auto& p : points) {
+      for (int j = 0; j < dimensions; j++) {
+        std::cout << p[j] << ", ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+private:
+  std::array<Point<dimensions>, N> p;
+  std::array<Point<dimensions>, (N-3)*(n_points+1)> points;
+  float tao;
+};
+
+#endif  // CATMULL_ROM_SPLINE_HPP
--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 
 #include "bezier_curve.hpp"
 #include "cubic_hermite_spline.hpp"
+#include "catmull_rom_spline.hpp"
 #include "curve_conversion.hpp"
+
+template<size_t dimensions>
+void print(std::array<Point<dimensions>, 4>& a){
+  for (const auto& p : a) {
+    std::cout << "( ";
+    for (const auto ele : p) {
+      std::cout << ele << ',';
+    }
+    std::cout << ")";
+  }
+  std::cout << '\n';
+}
 
 // int main() {
 //   std::array<Point<3>, 4> weights = {
@@ -29,44 +42,55 @@
 //   return 0;
 // }
 
-template<size_t dimensions>
-void print(std::array<Point<dimensions>, 4>& a){
-  for (const auto& p : a) {
-    std::cout << "( ";
-    for (const auto ele : p) {
-      std::cout << ele << ',';
-    }
-    std::cout << ")";
-  }
-  std::cout << '\n';
-}
+// int main() {
+//   constexpr int n_points = 100;
+//   constexpr int dimensions = 2;
+//   std::array<Point<dimensions>, 4> weights = {
+//     Point<dimensions>{110,150},//0},
+//     Point<dimensions>{25,190}, //1},
+//     Point<dimensions>{210,250}, //2},
+//     Point<dimensions>{210, 30} //3}
+//   };
+//   print(weights);
+//
+//   BezierCurve<3, n_points, dimensions> b (weights);
+//   b.print();
+//   std::cout << '\n';
+//
+//   std::array<Point<dimensions>, 4>  hermite_points = convertBezierPointsToHermitePoints(weights);
+//   print(hermite_points);
+//   CubicHermiteSpline<n_points, dimensions> hs (hermite_points);
+//   hs.print();
+//   std::cout << '\n';
+//
+//   std::array<Point<dimensions>, 4>  weights_returned = convertHermitePointsToBezierPoints(hermite_points);
+//   print(weights_returned);
+//   BezierCurve<3, n_points, dimensions> b_returned (weights_returned);
+//   b_returned.print();
+//   std::cout << '\n';
+//
+//   return 0
+// }
 
 int main() {
   constexpr int n_points = 10;
   constexpr int dimensions = 2;
-  std::array<Point<dimensions>, 4> weights = {
-    Point<dimensions>{110,150},//0},
-    Point<dimensions>{25,190}, //1},
-    Point<dimensions>{210,250}, //2},
-    Point<dimensions>{210, 30} //3}
+  constexpr int N = 9;
+  std::array<Point<dimensions>, N> p = {
+      Point<dimensions>{-1., -1.5},
+      Point<dimensions>{0, 0.5},
+      Point<dimensions>{1, 1.5},
+      Point<dimensions>{4, 3.2},
+      Point<dimensions>{3.5, -0.1},
+      Point<dimensions>{2.5, 0.7},
+      Point<dimensions>{-1., -1.5},
+      Point<dimensions>{0, 0.5},
+      Point<dimensions>{1, 1.5}
   };
-  print(weights);
 
-  BezierCurve<3, n_points, dimensions> b (weights);
-  b.print();
-  std::cout << '\n';
-  
-  std::array<Point<dimensions>, 4>  hermite_points = convertBezierPointsToHermitePoints(weights);
-  print(hermite_points);
-  CubicHermiteSpline<n_points, dimensions> hs (hermite_points);
-  hs.print();
-  std::cout << '\n';
-
-  std::array<Point<dimensions>, 4>  weights_returned = convertHermitePointsToBezierPoints(hermite_points);
-  print(weights_returned);
-  BezierCurve<3, n_points, dimensions> b_returned (weights_returned);
-  b_returned.print();
-  std::cout << '\n';
+  const float tao = 0.5;
+  CatmullRomSpline<N, n_points, dimensions> cms(p, tao);
+  cms.print();
 
   return 0;
 }

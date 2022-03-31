2a732a8aeb805c11aa06379682ca46caf9659a1a
vss <28676655+vss2sn@users.noreply.github.com>
2022-03-30 17:29:52 -0700

    Now coeffs are correctly calced

--- hermite_splines.hpp
+++ hermite_splines.hpp
@@ -22,14 +22,24 @@ private:
 
   template<size_t I>
   void compile_for(std::array<std::array<double, degree+1>, degree+1>& coeffecient_matrix_of_p_to_dnp) {
-    if constexpr  ((degree + 1)/2 <= I) {
+    if constexpr  (degree < I) {
       return;
     } else {
-      auto poly_at_i = Polynomial<degree>(coeffecient_matrix_of_p_to_dnp[I]);
-      // std::cout << I << ": ";
-      // poly_at_i.print();
-      coefficients[2*I] = poly_at_i.get_component_value(0.);
-      coefficients[2*I+1] = poly_at_i.get_component_value(1.);
+      std::cout << I/2 << ' ' << I%2 << '\n';
+      std::array<double, degree-I/2 + 1> temp;
+      std::copy(
+        std::begin(coeffecient_matrix_of_p_to_dnp[I/2]) + I/2,
+        std::end(coeffecient_matrix_of_p_to_dnp[I/2]),
+        std::begin(temp)
+      );
+      auto poly_at_i = Polynomial<degree - I/2>(temp);
+      auto temp2 = poly_at_i.get_component_value(double(I%2));
+      for (int i = 0; i < temp2.size(); i++) {
+        coefficients[I][i] = temp2[i];
+      }
+      for (int i = temp2.size(); i < coefficients[I].size(); i++) {
+        coefficients[I][i] = 0;
+      }
       compile_for<I+1>(coeffecient_matrix_of_p_to_dnp);
     }
   }
